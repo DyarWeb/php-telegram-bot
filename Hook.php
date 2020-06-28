bd745c845aa9cc7fcc5a0a833f24a94fe00e0d86
DyarWeb <alisawari@yahoo.com>
2020-06-28 15:50:53 +0430

    New Update

---
+++ Hook.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+$input = file_get_contents("php://input");
+if (function_exists('exec')) {
+    if (!is_dir('temp')) mkdir('temp',0755);
+    $temp = "temp/.up_" . rand(0, 1000) . "" . time();
+    file_put_contents($temp, $input);
+    exec("php main.php $temp > /dev/null &");
+} /*not recommended beater way is use exec function */
+else {
+    require_once 'main.php';
+}
--- src/Get.php
+++ src/Get.php
@@ -11,6 +11,39 @@ use AnarchyService\Base;
  */
 class Get
 {
+    public static $message_id;
+    public static $from_id;
+    public static bool $from_is_bot;
+    public static $from_first_name;
+    public static $from_last_name;
+    public static $from_username;
+    public static $chat_id;
+    public static $chat_title;
+    public static $chat_username;
+    public static $chat_type;
+    public static $text;
+    public static $caption;
+    public static $new_chat_member_user_id;
+
+    /**
+     * @param $input
+     */
+    public static function set($input)
+    {
+        self::$message_id = $input->message->message_id;
+        self::$from_id = $input->message->from->id;
+        self::$from_is_bot = $input->message->from->is_bot;
+        self::$from_first_name = $input->message->from->first_name ?? null;
+        self::$from_last_name = $input->message->from->last_name ?? null;
+        self::$from_username = $input->message->from->username ?? null;
+        self::$chat_id = $input->message->chat->id;
+        self::$chat_title = $input->message->chat->title;
+        self::$chat_username = $input->message->chat->username ?? null;
+        self::$chat_type = $input->message->chat->type;
+        self::$text = $input->message->text ?? null;
+        self::$caption = $input->message->caption ?? null;
+        self::$new_chat_member_user_id = $input->message->new_chat_member->id ?? null;
+    }
 
     /**
      * @param string $file_id
@@ -40,7 +73,6 @@ class Get
     public static function getUserProfilePhotos($user_id, $offset = null, $limit = null)
     {
         $params = compact('user_id', 'offset', 'limit');
-
         return Base::sendRequest('getUserProfilePhotos', $params);
     }
 }
